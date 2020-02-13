5e4ee867d023e8b9e64cc464cdd47b063f60975e
Jean-Olivier Dalphond <jean-olivier.dalphond@polymtl.ca>
2020-02-13 18:24:15 -0500

    Add files via upload

---
+++ src/server/Server.java
@@ -0,0 +1,181 @@
+package server;
+
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.imageio.ImageIO;
+
+public class Server {
+	private static ServerSocket listener;
+	private static KeyboardHandler handler;
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Bienvenue dans l'application PolySobel - Serveur! (Copyright Derek Bernard & Jean-Olivier Dalphond 2020)");
+		
+		int clientNumber = 0;
+		int serverPort = 0;
+		String serverAddress = "";
+        
+		serverAddress = Init.serverIP();
+		serverPort = Init.serverPort();
+
+		listener = new ServerSocket();
+		listener.setReuseAddress(true);
+		InetAddress serverIP = InetAddress.getByName(serverAddress);
+		
+		try {
+			listener.bind(new InetSocketAddress(serverIP, serverPort));
+		} catch (Exception e) {
+			System.out.println("Erreur lors de la liaison du socket. Socket deja en utilisation? Sortie de l'application...");
+			listener.close();
+			System.exit(1);
+		}
+		
+		Validators.manageCredentials();
+		System.out.format("Le serveur fonctionne sur %s:%d%n", serverAddress, serverPort);
+		
+		handler = new KeyboardHandler(new BufferedReader(new InputStreamReader(System.in)));
+		handler.start();
+		try
+		{
+			while (true)
+			{	
+				new ClientHandler(listener.accept(), clientNumber++).start();
+			}
+		} catch (Exception e) {
+		}
+		finally
+		{
+			handler.close();
+			listener.close();
+		}
+	}
+
+	private static class KeyboardHandler extends Thread {
+		private BufferedReader reader;
+		
+		public KeyboardHandler(BufferedReader input) {
+			this.reader = input;
+		}
+		
+		public void run() {
+			System.out.println("Le serveur est en attente d'une connexion. Vous pouvez taper 'q' puis 'Entree' pour quitter.");
+			try {
+				if (reader.readLine().equalsIgnoreCase("q")) {
+					handler.close();
+					listener.close();
+					System.out.println("Merci d'avoir utilise PolySobel - Serveur. A la prochaine!");
+					System.exit(0);
+				}
+			} catch (IOException e) {
+				System.out.println("Exception levee lors de la capture du clavier.");
+				e.printStackTrace();
+			}
+		}
+		
+		public void close() throws IOException {
+			reader.close();
+		}
+	}
+	
+	private static class ClientHandler extends Thread {
+		private Socket socket;
+		private int clientNumber;
+		
+		public ClientHandler(Socket socket, int clientNumber)
+		{
+			this.socket = socket;
+			this.clientNumber = clientNumber;
+			System.out.println("Nouvelle connexion avec le client #" + clientNumber + " sur " + socket + ".");
+		}
+		
+		public void run()
+		{
+			try
+			{
+				DataInputStream in = new DataInputStream(socket.getInputStream());
+				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+				String usernameIn = in.readUTF();
+				Boolean userExists = Validators.validateUsername(usernameIn);
+				out.writeBoolean(userExists);
+				String password = in.readUTF();
+				
+				if (userExists) {
+					if (Validators.validatePassword(usernameIn, password)) {
+						out.writeBoolean(true);
+						System.out.format("Usager existant %s s'est connecte. En attente d'une image...\n", usernameIn);
+					} else {
+						out.writeBoolean(false);
+						System.out.format("Usager existant %s: mot de passe refuse. \n", usernameIn);
+						return;
+					}
+				} else {
+					Validators.setPassword(usernameIn, password);
+					out.writeBoolean(true);
+					System.out.format("Nouvel usager %s s'est connecte. En attente d'une image...\n", usernameIn);
+				}
+				
+				// Attente de la taille et du nom de l'image
+				int len = in.readInt();
+				String imageName = in.readUTF();
+				
+				// Obtention de la date
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd@HH:mm:ss");
+				Date now = new Date();
+				
+				// Attente de l'image
+				System.out.format("Taille de l'image: %s octets. Reception d'image...\n", len);
+				byte[] inputImage = in.readNBytes(len);
+				
+				// Image recue
+				System.out.println("[" + usernameIn + " - " + Validators.cleanIPAddressFormat(socket.getRemoteSocketAddress().toString()) + ":" + socket.getLocalPort() + " - " + sdf.format(now) + "] : Image " + imageName + " recue pour traitement.");
+				InputStream inp = new ByteArrayInputStream(inputImage);
+				BufferedImage imageConverted = ImageIO.read(inp);
+				BufferedImage processedImage = Sobel.process(imageConverted);
+				
+				System.out.println("Image traitee. Transmission...");
+				ByteArrayOutputStream baOut= new ByteArrayOutputStream();
+				ImageIO.write(processedImage, "png", baOut);
+				byte[] lenMod = ByteBuffer.allocate(4).putInt(baOut.size()).array();
+				out.write(lenMod);
+				out.write(baOut.toByteArray());
+				out.flush();
+				System.out.println("Image envoyee au client.");
+			} catch (IOException e)	{
+				System.out.println("Erreur dans le traitement demande par le client # " + clientNumber + ": " + e);
+			} catch (Exception e) {
+				System.out.println("exception Exception Server.java run try catch");
+				e.printStackTrace();
+			}
+			finally
+			{
+				try
+				{
+					socket.close();
+					Validators.addToCredentials();
+				}
+				catch (IOException e)
+				{
+					System.out.println("Impossible de fermer le socket.");
+				} catch (Exception e) {
+					System.out.println("exception Exception Server.java run finally catch");
+					e.printStackTrace();
+				}
+				System.out.println("Connexion avec le client # " + clientNumber + " fermee.");
+			}
+		}
+	}
+}
---
+++ src/server/Validators.java
@@ -0,0 +1,110 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Validators {
+	static String path = "credentials.csv";
+	static HashMap<String, String> credentials = new HashMap<String, String>();
+
+	// Valider l'adresse (chiffres et nombre de points)
+	public static boolean validateIPAddress(String[] input) throws Exception {
+		if (input.length != 4) {
+			System.out.println("Erreur dans l'addresse. Veuillez reessayer.");
+			return false;
+		}
+		for (int i = 0; i < input.length; i++) {
+			try {
+				int tempInt = Integer.parseInt(input[i]);
+				if (tempInt < 0 || tempInt > 255) {
+					return false;
+				}
+			} catch (Exception e) {
+				System.out.println("Erreur dans l'addresse. Veuillez reessayer.");
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// Valider le numero de port
+	public static boolean validatePortNumber(int input) throws Exception {
+		if (input < 5000 || input > 5050) {
+			System.out.println("Erreur dans le numero de port. Veuillez reessayer.");
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean validateUsername(String username) throws Exception {
+		if (credentials.containsKey(username)) {
+			return true;
+		} else {
+			credentials.put(username, " ");
+		}
+		return false;
+	}
+	
+	public static boolean validatePassword(String username, String password) throws Exception {
+		if (credentials.get(username).equals(password)) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static void setPassword(String username, String password) throws Exception {
+		// On met a jour la map
+		credentials.put(username, password);
+		// On met a jour le fichier
+		addToCredentials();
+	}
+	
+	public static String cleanIPAddressFormat(String input) throws Exception {
+		String[] temp = input.split("\\/");
+		String[] temp2 = temp[1].split(":");
+		return temp2[0];
+	}
+	
+	public static void manageCredentials() throws Exception {
+		File file = new File(path);
+		// Le fichier existe deja
+		if (file.exists()) {
+			BufferedReader reader = new BufferedReader(new FileReader(path));
+			String line = "";
+			String[] data;
+			while ((line = reader.readLine()) != null) {
+				data = line.split(",");
+				credentials.put(data[0], data[1]);
+			}
+			reader.close();
+		// On doit creer le fichier
+		} else {
+			System.out.println("Fichier d'identifiants introuvable. Creation d'un nouveau fichier credentials.csv.");
+			addToCredentials();
+		};
+	}
+	
+	public static void addToCredentials() throws Exception {
+		FileWriter writer = new FileWriter(path);
+		// On vide le fichier et ecrit la nouvelle map
+		writer.write("");
+		credentials.forEach((username, password) -> {
+			// Une ligne par username
+			try {
+				writer.append(username);
+				writer.append(",");
+				writer.append(password);
+				writer.append("\n");
+			} catch (IOException e) {
+				System.out.println("Erreur lors de l'écriture vers le fichier de credentials.");
+				e.printStackTrace();
+			}
+		});
+		writer.close();
+	}
+}
+	
